0fdfa3694d556b37127510ed56e5f5a1dc219109
Dove0fPeace <lebedev07.98@gmail.com>
2022-11-11 15:26:48 +0500

    Button animation

--- Assets/Scripts/UI/ButtonAnim.cs
+++ Assets/Scripts/UI/ButtonAnim.cs
@@ -1,9 +1,15 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class ButtonAnim : MonoBehaviour
+public class ButtonAnim : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
 {
+    [SerializeField] private Color _defaultButtonColor;
+    [SerializeField] private Color _buttonColorOnSelect;
+
+    private TMP_Text _targetText;
     [SerializeField] private Vector3 _scaleUIElement = new Vector3(0.3f,0.3f,0.3f);
     [SerializeField] private float _durationScaleUI = 0.2f;
     
@@ -14,8 +20,11 @@ public class ButtonAnim : MonoBehaviour
     private void Start()
     {
         _transform = GetComponent<RectTransform>();
+        _targetText = transform.GetComponentInChildren<TMP_Text>();
+        _targetText.color = _defaultButtonColor;
         _button = GetComponent<Button>();
         _button.onClick.AddListener(ButtonAnimation);
+        
     }
 
     private void OnDestroy()
@@ -26,5 +35,17 @@ public class ButtonAnim : MonoBehaviour
     private void ButtonAnimation()
     {
         _transform.DOPunchScale(_scaleUIElement, _durationScaleUI);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _targetText.color = _buttonColorOnSelect;
+        _transform.DOScale(_scaleUIElement, _durationScaleUI);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _targetText.color = _defaultButtonColor;
+        _transform.DOScale(new Vector3(1,1,1), _durationScaleUI);
     }
 }
